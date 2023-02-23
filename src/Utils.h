2c69c00cf6c160353e18e17b2d40d6681e95983d
Ido Sharon <michdodim@gmail.com>
2023-02-23 09:00:43 +0200

    Added info macro

--- src/Utils.h
+++ src/Utils.h
@@ -5,6 +5,9 @@
 #include "Consts.h"
 #include "Types.h"
 
+#define info(msg) printf("[info]: %s", msg)
+#define info_file(msg, file_name) printf("[info]: %s: %s", msg, file_name)
+
 int appendBinaryToFile(FILE*, word);
 word calculateOnsComplete(word*);
 
